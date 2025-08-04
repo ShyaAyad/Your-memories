d1a002bc67d8f112cae111b56b65ee447b6baf7f
ShyaAyad <shyaayad1@gmail.com>
2025-08-04 21:49:53 +0300

    creating a memory is done, has a few bugs in fetching all data

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const dontenv = require('dotenv').config()
+const cors = require('cors')
 const { connectDB } = require('./config/db.js')
 
 const PORT = process.env.PORT || 5000
 
 const app = express()
+app.use(cors())
 
 app.use(express.json()) // middleware to parse json request 
 app.use(express.urlencoded({extended: false}))
